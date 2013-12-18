83512cb3b6a0e610133f75b0db13a1a75572a86b
a358003542 <a358003542@gmail.com>
2013-12-18 18:18:22 +0800

    python宏包小调整

---
+++ latex高级篇/person数据库-第一个例子/person数据库-1.py
@@ -0,0 +1,14 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+##################################
+#序言部分
+
+
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+

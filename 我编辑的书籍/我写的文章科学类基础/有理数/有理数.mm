90c7b11f87fcd07134f52692705ac73948729f28
a358003542 <a358003542@gmail.com>
2014-06-03 21:21:37 +0800

    201464前夕纪念防火墙好卡

---
+++ 我编辑的书籍/我写的文章科学类基础/有理数/有理数.mm
@@ -0,0 +1,111 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x6709;&#x7406;&#x6570;" LOCALIZED_STYLE_REF="default" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1401767651260" VGAP="0"><hook NAME="MapStyle" layout="OUTLINE">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" STYLE_REF="level1" LAST="false">
+            <node_level_condition VALUE="1" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" STYLE_REF="level2" LAST="false">
+            <node_level_condition VALUE="2" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" MIN_WIDTH="100" COLOR="#000000" BACKGROUND_COLOR="#ffffff" STYLE="as_parent" NUMBERED="true">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18" BOLD="false" ITALIC="false"/>
+<hook NAME="NodeConditionalStyles">
+    <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="default" LAST="false"/>
+</hook>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="level1" STYLE="bubble">
+<font SIZE="14"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode TEXT="level2" STYLE="bubble">
+<font SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<edge COLOR="#808080"/>
+<font SIZE="18"/>
+<node TEXT="&#x6709;&#x7406;&#x6570;&#x603b;&#x8bba;" POSITION="right" ID="ID_456055786" CREATED="1401685408003" MODIFIED="1401714691027"/>
+<node TEXT="&#x6bd4;&#x8f83;&#x5224;&#x65ad;" POSITION="right" ID="ID_709990960" CREATED="1401685408006" MODIFIED="1401767600151"/>
+<node TEXT="&#x52a0;&#x6cd5;" STYLE_REF="level1" POSITION="right" ID="ID_275037139" CREATED="1401685408006" MODIFIED="1401775537076">
+<hook NAME="NodeConditionalStyles">
+    <conditional_style ACTIVE="true" STYLE_REF="level1" LAST="false"/>
+</hook>
+</node>
+<node TEXT="&#x6570;&#x5e8f;&#x601d;&#x60f3;" STYLE_REF="level1" POSITION="right" ID="ID_903788089" CREATED="1401685408009" MODIFIED="1401691850811">
+<hook NAME="NodeConditionalStyles">
+    <conditional_style ACTIVE="true" STYLE_REF="level1" LAST="false">
+        <node_level_condition VALUE="1" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+    </conditional_style>
+</hook>
+</node>
+<node TEXT="&#x8fdb;&#x4f4d;" POSITION="right" ID="ID_900054848" CREATED="1401685408016" MODIFIED="1401775581693">
+<edge COLOR="#808080"/>
+<font SIZE="14"/>
+<node TEXT="&#x770b;&#x65f6;&#x95f4;" ID="ID_86311965" CREATED="1401685408018" MODIFIED="1401793330734"/>
+</node>
+<node TEXT="&#x51cf;&#x6cd5;&#x8d1f;&#x6570;&#x548c;&#x96f6;" POSITION="left" ID="ID_978123763" CREATED="1401793310416" MODIFIED="1401793316804"/>
+<node TEXT="&#x5206;&#x6570;" POSITION="left" ID="ID_1153099080" CREATED="1401793372206" MODIFIED="1401794112472"/>
+<node TEXT="&#x5355;&#x4f4d;" POSITION="left" ID="ID_160334834" CREATED="1401793382726" MODIFIED="1401794194784">
+<node TEXT="&#x6570;&#x94b1;" ID="ID_1644907725" CREATED="1401793434758" MODIFIED="1401794199999"/>
+</node>
+<node TEXT="&#x66f4;&#x5927;&#x7684;&#x6570;" POSITION="left" ID="ID_1982398322" CREATED="1401794206031" MODIFIED="1401794217913">
+<node TEXT="&#x9012;&#x63a8;" ID="ID_724618957" CREATED="1401794219831" MODIFIED="1401794222663"/>
+</node>
+<node TEXT="&#x4e58;&#x6cd5;&#x548c;&#x9664;&#x6cd5;" POSITION="left" ID="ID_539673331" CREATED="1401793440902" MODIFIED="1401793451606">
+<node TEXT="&#x4f7f;&#x7528;&#x8ba1;&#x7b97;&#x5668;" ID="ID_165438356" CREATED="1401794330646" MODIFIED="1401794333954"/>
+</node>
+<node TEXT="&#x7b80;&#x5355;&#x4ee3;&#x6570;" POSITION="left" ID="ID_134739906" CREATED="1401793454702" MODIFIED="1401793461078"/>
+</node>
+</map>
---
+++ 我编辑的书籍/我写的文章科学类基础/有理数/有理数总论/有理数总论.mm
@@ -0,0 +1,82 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="&#x6709;&#x7406;&#x6570;&#x603b;&#x8bba;" LOCALIZED_STYLE_REF="default" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1401767946206" VGAP="0"><hook NAME="MapStyle">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" STYLE_REF="level1" LAST="false">
+            <node_level_condition VALUE="1" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" STYLE_REF="level2" LAST="false">
+            <node_level_condition VALUE="2" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" MIN_WIDTH="100" COLOR="#000000" BACKGROUND_COLOR="#ffffff" STYLE="as_parent" NUMBERED="true">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#000000" BACKGROUND_COLOR="#ffffff">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18" BOLD="false" ITALIC="false"/>
+<hook NAME="NodeConditionalStyles">
+    <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="default" LAST="false"/>
+</hook>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="level1" STYLE="bubble">
+<font SIZE="14"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode TEXT="level2" STYLE="bubble">
+<font SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="12"/>
+<edge COLOR="#808080"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<edge COLOR="#808080"/>
+<font SIZE="18"/>
+<node TEXT="&#x603b;&#x8bba;" POSITION="right" ID="ID_262246495" CREATED="1401767934083" MODIFIED="1401767954137"/>
+<node TEXT="&#x8ba4;&#x8bc6;&#x8bba;" POSITION="left" ID="ID_1920623345" CREATED="1401767960865" MODIFIED="1401767964545"/>
+</node>
+</map>
